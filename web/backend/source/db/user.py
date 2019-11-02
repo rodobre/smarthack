287fe35cc705029624a1dc03794059e4a4605972
JustBeYou <mihailferaru2000@gmail.com>
2019-11-02 09:36:04 +0200

    Basic login routes

--- web/backend/source/db/user.py
+++ web/backend/source/db/user.py
@@ -1,11 +1,20 @@
 from . import Base, engine
 from sqlalchemy import Column, Integer, String
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(Base):
+class User(Base, UserMixin):
     __tablename__ = 'user'
 
     id = Column(Integer, primary_key = True)
     name = Column(String, nullable = False)
+    password_hash = Column(String, nullable = False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return "<User id={} name={}>".format(self.id, self.name)
