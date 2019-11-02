09b93282a487fa8f604096af74dc446e79c79de9
JustBeYou <mihailferaru2000@gmail.com>
2019-11-02 15:47:51 +0200

    db schema

---
+++ web/backend/source/db/family.py
@@ -0,0 +1,33 @@
+from . import Base, engine
+from sqlalchemy import Column, Integer, String
+
+class Family(Base):
+    __table__ = "family"
+
+    id = Column(Integer, primary_key = True)
+
+    caretakers = relationship("Caretaker", backref="family")
+    patients = relationship("Patients", backref="family")
+
+class Caretaker(Base):
+    __table__ = "caretaker"
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String)
+    desc = Column(String)
+    img  = Column(String)
+    phone = Column(String)
+
+    family_id = Column(Integer, ForeignKey('family.id'))
+
+class Patient(Base):
+    __table__ = "patient"
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String)
+    img = Column(String)
+    # add todo TODO
+
+    family_id = Column(Integer, ForeignKey('family.id'))
+
+Base.metadata.create_all(engine)
