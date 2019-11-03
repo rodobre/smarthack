44ce221aab40710a9d560a23efcc76a9cc1c0806
adrgs <albastroiudragos@gmail.com>
2019-11-03 04:04:07 +0200

    ssl

--- web/backend/source/app.py
+++ web/backend/source/app.py
@@ -112,5 +112,5 @@ def recognizefaces():
 # -------- Flask entrypoint ---------
 # -----------------------------------
 if __name__ == '__main__':
-	app.run(host='0.0.0.0')
+	app.run(ssl_context=('certificate.pem', 'key.pem'), host='0.0.0.0')
 # -----------------------------------
